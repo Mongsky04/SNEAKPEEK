a1b4aff020ec5f480a19668f9e40e25bd481c76c
LetsTray <letstraey@gmail.com>
2024-12-18 18:47:43 +0700

    modify admin pages

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { assets } from "../assets/assets";
 
 const Navbar = ({ setToken }) => {
@@ -8,6 +8,27 @@ const Navbar = ({ setToken }) => {
       <button
         onClick={() => setToken("")}
         className=" bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;*/
+
+
+import React from "react";
+import { assets } from "../assets/assets";
+import contactlogo from "../assets/contactlogo.png"
+
+const Navbar = ({ setToken }) => {
+  return (
+    <div className=" flex items-center py-2 px-[4%] justify-between">
+      <img src={contactlogo} alt="" className=" w-[max(10%, 80px)] size-48" />
+      <button
+        onClick={() => setToken("")}
+        className=" bg-neonPurple text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
       >
         Logout
       </button>
